26b6731d02eaa931198b2d30e996e15737acbebe
wangjz <17091267797@163.com>
2019-12-14 18:03:15 +0800

    数据源

--- spring-boot/spring-boot-mybatis/src/main/java/com/wjz/springboot/mapper/db1/VersionMapper.java
+++ spring-boot/spring-boot-mybatis/src/main/java/com/wjz/springboot/mapper/db1/VersionMapper.java
@@ -1,9 +1,7 @@
 package com.wjz.springboot.mapper.db1;
 
 import com.wjz.springboot.domain.Version;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public interface VersionMapper {
 
 	Version selectById(Integer id);
